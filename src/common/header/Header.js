896cdbc0fee193fc8fd83802b7f95fdd1db38660
illusionrs <rajeevshukla255@gmail.com>
2020-05-25 13:38:45 +0530

    login button added

---
+++ src/common/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <Button variant="contained">LogIn</Button>
+
+            </div>
+        )
+    }
+}
+export default Header;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../../common/header/Header';
 
 class Home extends React.Component{
     render(){
         return(
-            <div></div>
+            <div>
+                <Header/>
+            </div>
         )
     }
 }
