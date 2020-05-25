ff280a3bde0bcc430466c63cc795bad86dd10a84
illusionrs <rajeevshukla255@gmail.com>
2020-05-25 13:58:16 +0530

    header designed

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import './Header.css';
+
 
 
 class Header extends React.Component{
     render(){
         return(
-            <div>
-                <Button variant="contained">LogIn</Button>
+            <div className="header">
+                <Button variant="contained"  className="bt">LogIn</Button>
 
             </div>
         )
