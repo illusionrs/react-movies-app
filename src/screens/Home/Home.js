223462dad39a09bfd8cf3cbe80092b318ac04934
illusionrs <rajeevshukla255@gmail.com>
2020-05-28 13:19:21 +0530

    Added 'Movie Name' filter on home page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Home.css';
 import Header from '../../common/header/Header';
 import {withStyles} from '@material-ui/core/styles';
 import moviesData from '../../common/movieData';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import TypoGraphy from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 
 
@@ -24,10 +31,33 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
-    }
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+     },
+     title: {
+        color: theme.palette.primary.light,
+     }
+     
  });
 
 class Home extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            movieName:""
+        }
+    }
+    movieChangeHandler=event=>{
+        this.setState({movieName:event.target.value});
+    }
     render(){
         const {classes}=this.props;
         return(
@@ -40,7 +70,7 @@ class Home extends React.Component{
                     {
                         moviesData.map(movies=>(
                             <GridListTile key={movies.id}>
-                                <img src={movies.poster_url} alt={movies.title}/>
+                                <img src={movies.poster_url} className="movie-poster" alt={movies.title} />
                                 <GridListTileBar title={movies.title}/>
 
                             </GridListTile>
@@ -48,6 +78,37 @@ class Home extends React.Component{
                     }
 
                 </GridList>
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                        <Card>
+                            <CardContent>
+                            <FormControl className={classes.formControl}>
+                                <TypoGraphy className={classes.title} color="textSecondary">
+                                    FIND MOVIES BY:
+                                </TypoGraphy>
+                            
+                            </FormControl>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="movieName" >Movie Name</InputLabel>
+                                <Input id="movieName" onChange={this.movieChangeHandler}/>
+                            </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
